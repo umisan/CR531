6480e0d60b622801d3b855b3bfc6405a1fb11c64
umisan <t.umisan6@gmail.com>
2019-01-22 13:00:49 +0900

    e途中まで

--- CR531.cpp
+++ CR531.cpp
@@ -6,91 +6,29 @@
 
 using namespace std;
 using ll = long long int;
-
-int n;
-
-string increase_front(string s, int opt_num, vector<int> &numc, char target) {
-  int op = opt_num - numc[target - '0'];
-  for(int i = 0; i < n && op > 0; i++)
-  {
-    if(s[i] != target)
-    {
-      if(numc[s[i] - '0'] > opt_num){
-        numc[s[i] - '0']--;
-        s[i] = target;
-        numc[target - '0']++;
-        op--;
-      }
-    }
-  }
-  return s;
-}
-
-string increase_back(string s, int opt_num, vector<int> &numc, char target){
-  int op = opt_num - numc[target - '0'];
-  for(int i = n - 1; i >= 0 && op > 0; i--)
-  {
-    if(s[i] != target){
-      if(numc[s[i] - '0'] > opt_num) {
-        numc[s[i] - '0']--;
-        s[i] = target;
-        numc[target - '0']++;
-        op--;
-      }
-    }
-  }
-  return s;
-}
-
-string increase_1(string s, int opt_num, vector<int> &numc){
-  //replace 0 by 1
-  int op = opt_num - numc[1];
-  for(int i = n - 1; i >= 0 && op > 0; i--)
-  {
-    if(s[i] == '0' && numc[0] > opt_num) {
-      numc[0]--;
-      s[i] = '1';
-      numc[1]++;
-      op--;
-    }
-  }
-  //replace 2 by 1
-  op = opt_num - numc[1];
-  for(int i = 0; i < n && op > 0; i++)
-  {
-    if(s[i] == '2' && numc[2] > opt_num){
-      numc[2]--;
-      s[i] = '1';
-      numc[1]++;
-      op--;
-    }
-  }
-  return s;
-}
+using P = pair<ll, int>;
 
 int main() {
+  int n;
   cin >> n;
-  string s;
-  cin >> s;
-  vector<int> numc(3);
-  for(auto e : s)
+  vector<ll> a(n);
+  for(int i = 0; i < n; i++)
   {
-    numc[e - '0']++;
+    cin >> a[i];
   }
-  int opt_num = n / 3;
 
-  if(opt_num > numc[0]) {
-    s = increase_front(s, opt_num, numc, '0');
-    //cout << s << endl;
-  }
-  //cout << numc[0] << " " << numc[1] << " " << numc[2] << endl;
-  if(opt_num > numc[2])
+  //vector for find i, j such that ai == aj
+  vector<P> findv(n);
+  for(int i = 0; i < n; i++)
   {
-    s = increase_back(s, opt_num, numc, '2');
+    findv[i] = {a[i], i};
   }
-  if(opt_num > numc[1]){
-    s = increase_1(s, opt_num, numc);
+  sort(findv.begin(), findv.end());
+
+  ll result = 1;
+  ll mod = 998244353;
+  for(int i = 0; i < n; i++)
+  {
+
   }
-  cout << s << endl;
-  return 0;
 }
